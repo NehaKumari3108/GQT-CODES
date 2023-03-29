9a17b26c1a0c6bc6dcadad82f40748191d9bb49a
win10 <win10@DESKTOP-NINB9LP>
2023-03-29 21:24:53 +0530

    demo2

---
+++ GQTPROGRAMMS/src/Alphabet1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Alphabet1 {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+			Scanner in = new Scanner(System.in)) {
+				System.out.print("Enter the value : ");
+					int n = in.nextInt();
+					for(int i=0; i<=n-1;i++)
+					{
+						for(int j=0;j<=n-1;j++)
+						{
+					if(j==0||(j==n/2 && i!=0 && i!=n/2 && i!=(n-1))||(i==n/2 && j<n/2)||
+							(i==0 && j<n/2)||
+							(i==(n-1) && j<n/2))
+					  {
+						System.out.print("# ");
+					  }
+				  else
+				  {
+					  System.out.print("   ");
+				  }
+				}
+					System.out.println();
+				}
+			}
+		   }
+
+	}
+
