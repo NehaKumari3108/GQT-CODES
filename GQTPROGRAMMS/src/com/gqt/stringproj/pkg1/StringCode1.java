7e9e52cab429a97c13b971cf765c3842dfb79213
win10 <win10@DESKTOP-NINB9LP>
2023-04-08 12:23:25 +0530

    added String

---
+++ GQTPROGRAMMS/src/com/gqt/stringproj/pkg1/StringCode1.java
@@ -0,0 +1,16 @@
+package com.gqt.stringproj.pkg1;
+
+public class StringCode1 {
+
+	public static void main(String[] args) {
+		//using literal
+		String s1="Neha";
+		String s2=new String("Kumari");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+
+	}
+
+}
