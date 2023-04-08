12db89cff73a1fa6854347f46063f3735014c398
win10 <win10@DESKTOP-NINB9LP>
2023-04-08 18:12:57 +0530

    code is written

---
+++ GQTPROGRAMMS/src/com/gqt/stringproj/pkg1/StringCode3.java
@@ -0,0 +1,17 @@
+package com.gqt.stringproj.pkg1;
+
+public class StringCode3 {
+
+	public static void main(String[] args) {
+		String s1="Global";
+		String s2="Global";
+		if(s1==s2) {
+			System.out.println("Address are equal");
+		}
+		else {
+			System.out.println("Address are not equal");
+		}
+
+	}
+
+}
