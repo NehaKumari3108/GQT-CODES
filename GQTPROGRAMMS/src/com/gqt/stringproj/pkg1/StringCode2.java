448f5f9bda5425f9730dcd1b3b0e8edb34df58a4
win10 <win10@DESKTOP-NINB9LP>
2023-04-08 17:58:01 +0530

    equal strings

---
+++ GQTPROGRAMMS/src/com/gqt/stringproj/pkg1/StringCode2.java
@@ -0,0 +1,17 @@
+package com.gqt.stringproj.pkg1;
+
+public class StringCode2 {
+
+	public static void main(String[] args) {
+		String s1="Global";
+		String s2="Global";
+		if(s1.equals(s2)==true) {
+			System.out.println("Strings are equal");
+		}
+		else {
+			System.out.println("Strings are not equal");
+		}
+	}
+
+}
+
