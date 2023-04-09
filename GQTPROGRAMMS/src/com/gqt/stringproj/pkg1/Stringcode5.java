8268b9f34f81e0c32c8c121ff8ad78b948d47765
win10 <win10@DESKTOP-NINB9LP>
2023-04-09 20:20:26 +0530

    compare

---
+++ GQTPROGRAMMS/src/com/gqt/stringproj/pkg1/Stringcode5.java
@@ -0,0 +1,18 @@
+package com.gqt.stringproj.pkg1;
+
+public class Stringcode5 {
+
+	public static void main(String[] args) {
+		String s1="Global";
+		String s2="Global";
+		if(s1.compareTo(s2)==0) {
+			System.out.println("Strings are equal");
+		}
+		else {
+			System.out.println("Strings are not equal");
+			
+		}
+
+	}
+
+}
