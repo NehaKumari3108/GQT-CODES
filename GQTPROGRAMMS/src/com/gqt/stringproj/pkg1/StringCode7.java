36568eed7664d404278c4403a16bc2cc1d7369df
win10 <win10@DESKTOP-NINB9LP>
2023-04-13 00:42:55 +0530

    string concat

---
+++ GQTPROGRAMMS/src/com/gqt/stringproj/pkg1/StringCode7.java
@@ -0,0 +1,12 @@
+package com.gqt.stringproj.pkg1;
+
+public class StringCode7 {
+
+	public static void main(String[] args) {
+		String s1 = "sita";
+		String s2 = "ravana";
+		s1 = s1.concat(s2);
+		System.out.println(s1);
+	}
+
+}
